b9b2752132c26a5192d100cb4db7f9e532b19d17
Arju Mone <121635205+Arjumone@users.noreply.github.com>
2023-09-08 22:24:59 +0600

    added destructuring

--- src/User.jsx
+++ src/User.jsx
@@ -1,10 +1,12 @@
 export default function User({user}){
-    const {name,email,id}=user;
+    const {name,email,id,address}=user;
+    
     return (
         <div style={{border:'3px solid purple',borderRadius:'10px',padding:'15px',marginTop:'8px'}}>
             <h1>Id:{id}</h1>
             <h2>name:{name}</h2>
             <h2>Email:{email}</h2>
+            <h3>city:{address.city}</h3>
         </div>
     )
 }
